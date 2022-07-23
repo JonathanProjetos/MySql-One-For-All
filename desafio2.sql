7b8e0cb09c02f005377c7669cc812f0d34076262
Jonathan Santos <jonathan01347@gmail.com>
2022-07-23 11:02:24 -0300

    feat: Foi implementado a query para trazer a quantidade de tres tabelas diferentes

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT 
+	  COUNT(m.id) AS cancoes,
+    COUNT(c.id) AS artistas,
+    COUNT(a.id) AS albuns
+FROM SpotifyClone.Musicas AS m
+LEFT JOIN SpotifyClone.Cantor AS c
+ON m.id = c.id 
+LEFT JOIN SpotifyClone.Albuns AS a
+ON m.id = a.id;
